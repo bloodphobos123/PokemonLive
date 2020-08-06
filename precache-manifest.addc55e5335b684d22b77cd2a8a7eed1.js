906ad69761f3f8c0654e2e0f7372fd8b22fb1a4d
Grig Sargsyan <g.sargsyan92@gmail.com>
2020-08-06 11:19:27 +0400

    Add files via upload

---
+++ precache-manifest.addc55e5335b684d22b77cd2a8a7eed1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "716c346ee058301521a865e0a6e82d23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab536c5aa55d1b05818c",
+    "url": "/static/css/main.f31292a8.chunk.css"
+  },
+  {
+    "revision": "f903693d90291eb14a08",
+    "url": "/static/js/2.b959b53e.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/2.b959b53e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab536c5aa55d1b05818c",
+    "url": "/static/js/main.b56b3dbc.chunk.js"
+  },
+  {
+    "revision": "17b048e4ff9b4b55812c",
+    "url": "/static/js/runtime-main.124e427d.js"
+  },
+  {
+    "revision": "2879ea48049f5aaf9f87f7f28f7c2ccc",
+    "url": "/static/media/backgroundImage.2879ea48.jpg"
+  },
+  {
+    "revision": "29641507e615974b3f20177540a3e6a7",
+    "url": "/static/media/dontFavorit.29641507.jpg"
+  },
+  {
+    "revision": "a181d7e9c07aff7494640084931d24d9",
+    "url": "/static/media/favorit.a181d7e9.svg"
+  },
+  {
+    "revision": "04b34b0d00c86a6abdda75fc97498e63",
+    "url": "/static/media/favoritActive.04b34b0d.svg"
+  },
+  {
+    "revision": "eeec63d571662b71185d15b2b868092e",
+    "url": "/static/media/loader.eeec63d5.gif"
+  },
+  {
+    "revision": "b451cf45633cbc4426f20d92efeb5615",
+    "url": "/static/media/loaderPokemon.b451cf45.gif"
+  },
+  {
+    "revision": "8d5e7c5b2f6dab474e7e2e828e79dcef",
+    "url": "/static/media/logo.8d5e7c5b.png"
+  },
+  {
+    "revision": "8f99ae92077747be28d8197cfb927a1f",
+    "url": "/static/media/pokiDokiKorona.8f99ae92.png"
+  }
+]);
